01af7eaa7c6137b7a65d89d339f61b2fe7baee60
Denisha160 <denishaullas10@gmail.com>
2026-04-04 16:06:19 +0530

    UI changes

--- src/pages/common/kits/KitsPage.tsx
+++ src/pages/common/kits/KitsPage.tsx
@@ -387,7 +387,7 @@ const KitsPage = () => {
             </div>
           ) : items.length > 0 ? (
             <TooltipProvider>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 3xl:grid-cols-7 gap-3">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-7 gap-3">
                 {items.map((kit) => (
                   <Card
                     key={kit.id}
--- src/pages/common/products/ProductGridView.tsx
+++ src/pages/common/products/ProductGridView.tsx
@@ -60,7 +60,7 @@ const ProductGridView = ({
 
   return (
     <TooltipProvider>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 3xl:grid-cols-7 gap-3 p-1 animate-in fade-in duration-500">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 3xl:grid-cols-7 gap-3 p-1 animate-in fade-in duration-500">
         {items.map((product) => (
           <Card
             key={product.id}
